5270387e6a4774fc0d245702f44bea6964baa630
MaReinaCookGo <explorabit@MacBook-Pro-de-Explorabit.local>
2025-07-30 02:57:15 -0400

    Agrega test que fallará para verificar errores en CI

--- tests/playwright-home.spec.js
+++ tests/playwright-home.spec.js
@@ -4,3 +4,7 @@ test("Playwright homepage has correct title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
   await expect(page).toHaveTitle(/Playwright/);
 });
+test("Este test fallará", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await expect(page).toHaveTitle(/NoCoincideConElTitulo/); // Esto va a fallar
+});
